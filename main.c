cdad360db8b8a5cf17b594203b4be46275b4ab91
massygh <massyghen1@gmail.com>
2023-11-20 04:36:45 -0800

    test

--- main.c
+++ main.c
@@ -2,6 +2,6 @@
 
 int main(void)
 {
-    _printf("Hello, %c! This is a %s example with a %%.\n", 'W', "simple printf");
+    _printf("Hello, %c! This is a %s example with a %%.\n", 'I', "ines printf");
     return (0);
 }
