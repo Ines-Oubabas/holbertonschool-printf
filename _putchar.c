8d231fba9aea66f175f2ac861d59853f516dfbc7
massygh <massyghen1@gmail.com>
2023-11-21 05:08:30 -0800

    test

--- _putchar.c
+++ _putchar.c
@@ -1,6 +1,12 @@
 #include <unistd.h>
 
+/**
+ * _putchar - Écrit un caractère sur la sortie standard (STDOUT).
+ * @c: Le caractère à imprimer.
+ * Return: Le nombre de caractères écrits (1 si réussi, -1 en cas d'erreur).
+ */
+
 int _putchar(char c)
 {
-	return write(1, &c, 1);
+	return write(1, &c, 1); // Utilise la fonction système write pour écrire le caractère sur STDOUT.
 }
