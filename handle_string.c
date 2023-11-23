2216253ae6ba1be895becd0ee152ba85fd21596d
massygh <massyghen1@gmail.com>
2023-11-23 01:49:43 -0800

    return null

--- handle_string.c
+++ handle_string.c
@@ -11,16 +11,11 @@
 int handle_string(va_list args) 
 {	
 	char *str = va_arg(args, char *);
-	int i;
 	int count = 0;
-	char null_str[] = "(null)";
 
 	if (str == NULL)
 	{
-		for (i = 0; null_str[i] != '\0'; i++)
-		{
-			count += _putchar(null_str[i]);
-		}
+		return (-1);
 	}
 	else
 	{
